006d75dc471434f2bf90539ca9acd78793d3fc16
Padre <kuharukpavel@gmail.com>
2023-05-10 14:29:02 +0300

    Создание модуля DataBaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MinioClientModule } from './minio-clinet/minio-client.module';
 import {ConfigModule} from "@nestjs/config";
+import {DataBaseModule} from "./data-base/data-base.module";
 
 @Module({
   imports: [
     MinioClientModule,
+    DataBaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
