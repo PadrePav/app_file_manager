4a8011451d098e792268ecbbef2ca9065af97918
Padre <kuharukpavel@gmail.com>
2023-05-25 18:27:49 +0300

    Changing the DTO

--- src/data-base/dto/file.dto.ts
+++ src/data-base/dto/file.dto.ts
@@ -1,4 +1,6 @@
 import * as Stream from "stream";
+import {ApiProperty} from "@nestjs/swagger";
+import {IsNotEmpty, IsString} from "class-validator";
 
 export class FileDto {
   name: string;
--- src/data-base/dto/return.folder.dto.ts
+++ src/data-base/dto/return.folder.dto.ts
@@ -2,8 +2,14 @@ import {Folder} from "../entity/folder.entity";
 import {File} from "../entity/file.entity";
 
 class ReturnFolderDto {
+  folderId: string
   folders: Folder[];
   files: File[];
+}
+
+export class PathParentFolderDto {
+  path: string;
+  folderName: string;
 }
 
 export default ReturnFolderDto
